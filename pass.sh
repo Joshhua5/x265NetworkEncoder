9aa890fe69620f227e2951d90a82748c7f3bda26
Joshua Waring <Joshhua123@gmail.com>
2015-06-03 14:14:48 +0800

    Export from test server

--- pass.sh
+++ pass.sh
@@ -12,13 +12,15 @@ cd enc_ftp/Clients
 for dir in $(ls)
 do
 	cd dir
-	if[ $(ls done.txt) == "" ]; then
-		if[ $(ls tast.txt) == "" ]; then
-		# pass the file
-		mv "$1" "$(basename $1)"
-		"$1" | original_path.txt
-		"$(basename $1)" | tast.txt
-		exit
+	if ls done.txt
+		then
+		if ls tast.txt
+			then
+			# pass the file
+			mv "$1" "$(basename $1)"
+			"$1" | original_path.txt
+			"$(basename $1)" | tast.txt
+			exit
 		fi
 	fi 
 	cd ..
--- prepare_clients.sh
+++ prepare_clients.sh
@@ -2,16 +2,15 @@
 
 cd enc_ftp/Clients
 
-./prepare_clients
-
 #Cycle through all clients
 for dir in $(ls)
 do
-	cd dir
-	if[ $(ls done.txt) != "" ]; then 
-			mv -f "$(more task.txt)" "$(more original_path.txt).mp4"
-			rm -rf *
-	fi 
+	cd $dir
+	if ! ls done.txt
+	then
+		mv -f "$(more task.txt)" "$(more original_path.txt).mp4"
+		rm -rf *
+	fi
 	cd ..
 done
 
